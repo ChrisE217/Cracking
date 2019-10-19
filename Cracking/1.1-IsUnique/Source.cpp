8f57d1deb9b5d9f2889448cc72f559fc9a224b98
Chris Ewart <chris.ewart1999@gmail.com>
2019-10-19 15:26:44 +0100

    1.1

---
+++ Cracking/1.1-IsUnique/Source.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+
+bool isUniqueChars(string str);
+
+int main()
+{
+	string unique = "abcdefghijklmnopqrstuvwxyz";
+	string nonUnique = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+
+	bool t = isUniqueChars(nonUnique);
+
+	std::boolalpha;
+
+	cout << "Unique string: " << isUniqueChars(unique) << endl;
+	cout << "Non unique string: " << isUniqueChars(nonUnique) << endl;
+
+	return 0;
+}
+
+bool isUniqueChars(string str) {
+	if (str.length() > 128) return false; //Assuming ASCII string is used (256 for Extended ASCII)
+
+	bool arr[128] = { 0 };
+
+	for (int i = 0; i < str.length(); i++) {
+		int val = str[i];
+
+		if (arr[val]) return false;
+
+		arr[val] = true;
+	}
+
+	return true;
+}
